1010ef2f0cf5dc07910f19098a5a0dcd7a0b9df1
hfy2025 <hongfy2025@gmail.com>
2026-01-16 19:47:21 +0800

    Enhance customize.sh with Docker and backup features

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -49,7 +49,7 @@ ETH_COUNT=\$(ls -1 /sys/class/net/ | grep -E '^eth[0-9]+$' | wc -l)
 
 if [ "\$ETH_COUNT" -gt 1 ]; then
     # 多网口设备，设置固定IP
-    echo "多网口设备，设置固定IP: $MANAGEMENT_IP"
+    echo "多网口设备 (\$ETH_COUNT 个网口)，设置固定IP: $MANAGEMENT_IP"
     uci set network.lan.ipaddr='$MANAGEMENT_IP'
     uci set network.lan.proto='static'
     uci set network.lan.netmask='255.255.255.0'
@@ -67,9 +67,12 @@ EOF
 chmod +x files/etc/uci-defaults/99-set-network
 
 # 根据Luci版本调整 feeds.conf.default
-if [ -f feeds.conf.default.backup ]; then
-    cp feeds.conf.default.backup feeds.conf.default
+if [ -f feeds.conf.default.bak ]; then
+    cp feeds.conf.default.bak feeds.conf.default
 fi
+
+# 创建 feeds.conf.default 的备份
+cp feeds.conf.default feeds.conf.default.bak
 
 case $LUCI_VERSION in
     "22.03")
@@ -81,25 +84,37 @@ case $LUCI_VERSION in
         sed -i 's/openwrt-23.05/master/g' feeds.conf.default
         ;;
     *)
-        echo "使用默认 Luci 版本"
+        echo "使用默认 Luci 版本 24.10.5"
         ;;
 esac
+
+# 添加 Docker 配置
+if [ "$INSTALL_DOCKER" = "true" ]; then
+    echo "添加 Docker 支持..."
+    echo "# Docker 配置" >> .config
+    echo "CONFIG_PACKAGE_docker=y" >> .config
+    echo "CONFIG_PACKAGE_dockerd=y" >> .config
+    echo "CONFIG_PACKAGE_docker-compose=y" >> .config
+    echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> .config
+    echo "CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y" >> .config
+    
+    # Docker 依赖
+    echo "CONFIG_PACKAGE_kmod-veth=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-dm=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-br-netfilter=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-ikconfig=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-nf-conntrack-netlink=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-nf-ipvs=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-nf-nat=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-ipt-ipset=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-fs-btrfs=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-fs-overlay=y" >> .config
+    echo "CONFIG_PACKAGE_kmod-dax=y" >> .config
+fi
 
 # 添加应用商店
 if [ "$INSTALL_STORE" = "true" ]; then
     echo "添加应用商店支持"
-    if [ ! -d "package/thirdparty/luci-app-store" ]; then
-        git clone --depth=1 https://github.com/linkease/istore.git package/thirdparty/istore
-        # 创建符号链接
-        if [ ! -L "package/luci-app-store" ] && [ ! -d "package/luci-app-store" ]; then
-            ln -s ../thirdparty/istore/luci/luci-app-store package/
-        fi
-        if [ ! -L "package/luci-lib-xterm" ] && [ ! -d "package/luci-lib-xterm" ]; then
-            ln -s ../thirdparty/istore/luci/luci-lib-xterm package/
-        fi
-    fi
-    
-    # 在 .config 中添加应用商店
     echo "CONFIG_PACKAGE_luci-app-store=y" >> .config
     echo "CONFIG_PACKAGE_luci-lib-xterm=y" >> .config
 fi
