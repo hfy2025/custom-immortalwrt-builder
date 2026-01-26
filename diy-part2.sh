4586181934c30d7eb545a391cf32ec5abfd3414f
hfy2025 <hongfy2025@gmail.com>
2026-01-26 13:53:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,16 @@
-# 编译后自定义脚本
 #!/bin/bash
+cd openwrt
 
-# DIY 脚本第二部分 - 编译后执行
+# 1. 添加第三方插件源 (示例：Passwall)
+# 编辑 feeds.conf.default 文件，或在脚本中添加
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;lede-x86' >> feeds.conf.default
 
-echo "========== DIY Part 2 Start =========="
+# 2. 再次更新并安装所有 feeds (包括第三方)
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-# 修改管理 IP 和主机名
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/X86-Router/g' package/base-files/files/bin/config_generate
+# 3. 手动添加 Argon 主题
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
-echo "========== DIY Part 2 Complete =========="
+# 4. 安装编译所需工具（如ethtool）的包定义（如果需要从源码编译）
+# 通常直接在后缀的 .config 文件中选中即可
